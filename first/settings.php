1eede552b7dca5370fb09c3158a246114f4e90ca
pantobp <nemanjapantovic@live.ac.me>
2015-03-02 17:00:58 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="7011 West Archer Ave. Chicago, IL 60638"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Chicago";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=7733554840;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
